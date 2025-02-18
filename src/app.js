e3c4026c18e92257c56231d3057a83269a75609a
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-02-19 02:13:17 +0900

    app.js 루트 라우트 추가

--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ app.use(cors()); // 모든 도메인에서의 요청을 허용
 // 서버 실행
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
 // 라우트 추가
 app.use('/api/posts', postRoutes);
 app.use('/api/image', imageRoutes);
