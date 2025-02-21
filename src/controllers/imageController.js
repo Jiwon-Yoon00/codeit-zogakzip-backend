1992d78cdcb4d38323b60275011d1ef826d14261
JEREMIAHPARK99 <155166117+JEREMIAHPARK99@users.noreply.github.com>
2025-02-21 19:26:38 +0900

    Update imageController.js

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -1,36 +1,25 @@
 import multer from 'multer';
 import path from 'path';
 
-// 파일 저장 설정
+const imageFileFilter = (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png/;
+    const extension = path.extname(file.originalname).toLowerCase();
+    if (allowedTypes.test(extension)) {
+        cb(null, true);
+    } else {
+        cb(new Error('Only JPEG and PNG files are allowed!'), false);
+    }
+};
+
 export const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/'); // 이미지를 저장할 폴더
+        cb(null, './uploads');
     },
     filename: function (req, file, cb) {
+        const originalName = file.originalname.split('.')[0];
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname)); // 파일 이름 설정
+        cb(null, `${originalName}-${uniqueSuffix}${path.extname(file.originalname)}`);
     }
 });
 
-export const upload = multer({ storage: storage }).single('image'); // 'image'는 폼 데이터의 필드 이름
-
-export const uploadImage = (req, res) => {
-    upload(req, res, function (err) {
-        if (err) {
-            return res.status(500).json({ message: '이미지 업로드 실패', error: err.message });
-        }
-
-        // 파일이 제대로 업로드되었는지 확인
-        if (!req.file) {
-            return res.status(400).json({ message: '이미지가 업로드되지 않았습니다' });
-        }
-
-        // 이미지 URL 생성 (예: 서버의 도메인 + 이미지 경로)
-        const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
-        res.status(200).json({ imageUrl: imageUrl });
-    });
-};
-
-export default {
-    uploadImage,
-};
+export const upload = multer({ storage: storage, fileFilter: imageFileFilter }).single('image');
