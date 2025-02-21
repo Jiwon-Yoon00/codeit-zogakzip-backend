7d41fee76dfddb03f280aeba51694d82da6e350e
JEREMIAHPARK99 <155166117+JEREMIAHPARK99@users.noreply.github.com>
2025-02-22 00:41:52 +0900

    Update imageController.js

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -1,5 +1,6 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs'; // Import the filesystem module to manage directories.
 
 const imageFileFilter = (req, file, cb) => {
     const allowedTypes = /jpeg|jpg|png/;
@@ -13,7 +14,16 @@ const imageFileFilter = (req, file, cb) => {
 
 export const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads');
+        const uploadDir = './uploads'; // Define the directory name for uploads.
+
+        // Check if the directory exists.
+        if (!fs.existsSync(uploadDir)) {
+            // If the directory does not exist, create it.
+            fs.mkdirSync(uploadDir, { recursive: true });
+        }
+
+        // Pass the directory to the callback, it now definitely exists.
+        cb(null, uploadDir);
     },
     filename: function (req, file, cb) {
         const originalName = file.originalname.split('.')[0];
