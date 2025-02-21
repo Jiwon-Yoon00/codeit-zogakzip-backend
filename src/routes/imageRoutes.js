2d3d9cf4a6abb2a3dee686f7836ca0f4782d6999
JEREMIAHPARK99 <155166117+JEREMIAHPARK99@users.noreply.github.com>
2025-02-22 00:43:18 +0900

    Update imageRoutes.js

--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { uploadImage } from "../controllers/imageController.js";
+import { uploadImage } from "../controllers/imageController.js"; // .js 확장자 추가
+
 const router = express.Router();
 
 // POST /api/image - 이미지 업로드
